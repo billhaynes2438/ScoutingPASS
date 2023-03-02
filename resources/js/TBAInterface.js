b837465bcb13c930a3fe3a1794dd46093e781de9
billhaynes2438 <126732829+billhaynes2438@users.noreply.github.com>
2023-03-01 22:14:50 -0500

    Update TBAInterface.js
    changed TBA API read key to Bill Haynes

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "XmwC7aWay2ATCwShaE8TpfFJmxGk8Xf31XE1oO50eO99opmGFPiNzg5PgC7I55wd";
 /**
  * Get list of teams in event
  *

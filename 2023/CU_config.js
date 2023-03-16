5483a0dd09b68378cdb74378ba5039f9144a79f6
billhaynes2438 <126732829+billhaynes2438@users.noreply.github.com>
2023-03-16 19:32:02 -0400

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023midtr",
+      "defaultValue": "2023mimus",
       "required": "true",
       "disabled": "true"
     },

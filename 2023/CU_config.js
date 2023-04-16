ff00726de2cb1494790128b445a6fdfb28790cf7
billhaynes2438 <126732829+billhaynes2438@users.noreply.github.com>
2023-04-15 21:09:18 -0400

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023micmp3",
+      "defaultValue": "2023npfcmp",
       "required": "true",
       "disabled": "true"
     },

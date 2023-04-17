17fff7fc7118908a0dcb38d1ccbb026b0a76447f
billhaynes2438 <126732829+billhaynes2438@users.noreply.github.com>
2023-04-17 13:21:39 -0400

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023npfcmp",
+      "defaultValue": "2023new",
       "required": "true",
       "disabled": "true"
     },
